5b78ed0f42acb35ce57bdffe488c90c93922d9aa
rptr <wasmus@zom.bi>
2019-05-31 16:31:26 +0200

    add sd remote protocol stubs

--- sdremote.py
+++ sdremote.py
@@ -2,3 +2,30 @@
 # Impress Remote Control protocol impl
 #
 
+def SDRemoteClient():
+    def __init__(self);
+        self.socket = None
+
+    def transition_next(self):
+        pass
+
+    def transition_previous(self):
+        pass
+
+    def goto_slide(self):
+        pass
+
+    def slide_number(self):
+        pass
+
+    def presentation_start(self):
+        pass
+
+    def presentation_stop(self):
+        pass
+
+    def presentation_resume(self):
+        pass
+
+    def presentation_blank_screen(self):
+        pass
