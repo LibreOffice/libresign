f49b48332792790c94f3882a2a00e6795bb72e44
rptr <wasmus@zom.bi>
2019-06-26 21:15:21 +0200

    add cmd line arg to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if $1; then 
+    echo "./run.sh /path/to/libreoffice"
+    exit 1
+fi
+
+libo=$1
+
+
+
 source env/bin/activate
 
-libo=/media/compd/libreoffice
 # export PYTHONPATH=$libo:$libo/instdir/program
 export PYTHONPATH=$libo
 export LD_LIBRARY_PATH=$libo
