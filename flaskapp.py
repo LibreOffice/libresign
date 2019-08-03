b9f86cee91aa619855fc81b6f1c53d549c37bb43
rpj <wasmus@zom.bi>
2019-08-03 12:49:13 +0200

    change to MPL only

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-# Version: MPL 1.1/LGPL 2.1
+# Version: MPL 1.1
 #
 # This file is part of the LibreOffice project.
 #
@@ -14,17 +14,6 @@
 #
 # Contributor(s):
 # Rasmus P J <wasmus@zom.bi>
-#
-# Alternatively, the contents of this file may be used under the terms of
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL or the LGPL.
 #
 import os
 from flask import Flask, request, render_template, redirect, send_file
