6d942afab126eabeabf49503c81c4343073158c8
rpj <wasmus@zom.bi>
2019-08-08 20:08:31 +0200

    fix setup.sh git clone

--- libresign/setup.sh
+++ libresign/setup.sh
@@ -2,7 +2,7 @@
 
 mkdir presentations
 touch playlist
-git clone https://github.com:rptr/irpjs.git
+git clone https://github.com/rptr/irpjs.git
 virtualenv -p python3 env
 source env/bin/activate
 pip3 install Flask python-libxdo gevent-websocket
