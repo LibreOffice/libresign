e17a24ef64fe8a21d0928096fc5359f916ede6cf
rpj <wasmus@zom.bi>
2019-08-19 03:36:16 +0200

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = ""
 
 setuptools.setup(
     name="libresign",
-    version="0.1.0",
+    version="0.1.1",
     author="Rasmus P J",
     author_email="wasmus@zom.bi",
     description="Digital signage solution for LibreOffice.",
