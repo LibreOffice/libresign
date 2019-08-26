d4ba439b48df7b9067b0df36530a474d1e1fd52a
rpj <wasmus@zom.bi>
2019-08-26 06:46:04 +0200

    version 1.0.0
    hesitant to jump from 0.1.x to 1.0.0 but i guess that's how
    versioning works!

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = ""
 
 setuptools.setup(
     name="libresign",
-    version="0.1.1",
+    version="1.0.0",
     author="Rasmus P J",
     author_email="wasmus@zom.bi",
     description="Digital signage solution for LibreOffice.",
